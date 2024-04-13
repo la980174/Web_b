37b6989322d4e53a4c98885f8dfbe9350818872e
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2024-04-13 18:12:26 +0900

    Add files via upload

---
+++ week 1/실습2/index2.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const modal = document.getElementById(".modal-wrapper");
+
+open.onclick = () => {
+    modal.style.display = "flex";
+};
+
+close.onclick = () => {
+    modal.style.display = "none";
+};
